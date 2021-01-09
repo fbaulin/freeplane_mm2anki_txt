cf6dfd841ea4eb451024091b995ca67ad54cdc47
F.Baulin <59484776+fbaulin@users.noreply.github.com>
2021-01-09 15:55:07 +0300

    comand line UI and help for it

--- mm2txt.py
+++ mm2txt.py
@@ -8,26 +8,39 @@ from xml.etree import ElementTree as et
 PARSE_MODE = 'STYLE'                            # parsing mode TEXT or STYLE
 EXCLUDE_CONDITION = 'defaultstyle.floating'     # names of the style or node texts preference
 JOIN_UPSTREAM = 'styles.subtopic'               # name of the style or node text prefix
-TXT_TEMPLATE = '<div align="left">{0}</div>	<div align="left">{1}</div>'
+TXT_TEMPLATE = '<div align="left">{0}</div>\t<div align="left">{1}</div>'
 
 
-def auto_init(filename):
+def auto_mode_select(filename):
     with open(filename,'r') as f:
         txt = f.read()
-        global PARSE_MODE
-        global EXCLUDE_CONDITION
-        global JOIN_UPSTREAM
         if len(re.findall('TEXT=":ex ', txt)) >= 6 or (len(re.findall('TEXT=": ', txt)) >= 3 and 
         len(re.findall('="styles.subtopic"', txt)) <= 3 and 
         len(re.findall('="defaultstyle.floating"', txt)) <= 3):
-            PARSE_MODE = 'TEXT'
-            EXCLUDE_CONDITION = ':ex'
-            JOIN_UPSTREAM = ':'
+            mode = 'TEXT'
         else:
-            PARSE_MODE = 'STYLE'
-            EXCLUDE_CONDITION = 'defaultstyle.floating'
-            JOIN_UPSTREAM = 'styles.subtopic'
-        print('Automatic setup: \n\tmode: {}\n\texluding rule: {}\n\textra upstream node: {}'.format(PARSE_MODE,EXCLUDE_CONDITION,JOIN_UPSTREAM))
+            mode = 'STYLE'
+        print('Automatic mode selection')
+        return mode
+
+def parse_init(parse_mode='TEXT', join_condition=None, exclude_condition=None):
+    if parse_mode not in ['TEXT', 'STYLE']:
+        raise ValueError('Unsupported mode of parsing')
+    global PARSE_MODE
+    global EXCLUDE_CONDITION
+    global JOIN_UPSTREAM
+    PARSE_MODE = parse_mode
+    if isinstance(join_condition, str):
+        JOIN_UPSTREAM = join_condition
+    else:
+        JOIN_UPSTREAM = ':' if parse_mode == 'TEXT' else 'styles.subtopic'
+    if isinstance(exclude_condition, str):
+        EXCLUDE_CONDITION = exclude_condition
+    else:
+        EXCLUDE_CONDITION = ':ex' if parse_mode == 'TEXT' else 'defaultstyle.floating'
+    print('Parse setup: \n\tmode: {}\n\texluding rule: {}\n\textra upstream node: {}'.format(PARSE_MODE,EXCLUDE_CONDITION,JOIN_UPSTREAM))
+
+
 
 def node_keep(nodes):
     if PARSE_MODE == 'STYLE': 
@@ -89,19 +102,68 @@ def txt_encoder(filename, deck_pairs):
         for a, b in deck_pairs:
             print(TXT_TEMPLATE.format(a,b), file=f)
 
-def main(filename='default.mm', parse_mode=None):
-    # if filename is 
+def main():
+    """ Transform freeplane mindmaps to text for anki import
+    example:
+    python mm2txt.py [-idt] filename [exclude_condition [join_condition]]
+
+    outputs file with filename_deck.txt is in same directory as the source mindmap
+    (import in anki as text file with tab separated cards and html support)
+
+    OPTIONS: 
+    -t  text mode of parsing
+    -s  style mode of parsing
+    -i  inverse cards
+    -d  doublicate inversed cards (works only with i)
+    PARAMETERS:
+    filename          - name of freeplane mindmap file
+    exclude_condition - text prefix in node name (or style) of nodes you want to exclude from decks
+                        (use only with manual selection of parsing mode with -t or -s). 
+                        Lookup style names in the xml text of mindmap.
+    join_condition    - text prefix in node name (or style) of nodes you want to join with parent node text
+                        in order to support more info for insufficient 
+                        (use only with manual selection of parsing mode with -t or -s).
+                        Lookup style names in the xml text of mindmap.
+    
+    всем добра
+    """
+    filename = None
+    parse_mode = None
+    inverse = False
+    doublicate = False
+    join_condition = None
+    exclude_condition = None
+
+    n_args = len(sys.argv)-1
+    if n_args >= 1:     # aditional args
+        shell_args = sys.argv[1:]
+        # options section
+        if shell_args[0].startswith('-'):       # there are options
+            options = shell_args.pop(0)[1:]     # extract options
+            n_args -= 1                         # 1st arg was option
+            if 't' in options or 's' in options:    # if parsing mode specified
+                parse_mode = 'STYLE' if 's' in options else 'TEXT'  # set mode
+            inverse = True if 'i' in options else False
+            doublicate = True if 'd' in options else False
+        filename = shell_args[0] if n_args > 0 else 'default.mm'
+        exclude_condition = shell_args[1] if n_args > 1 else None
+        join_condition = shell_args[2] if n_args > 2 else None
+        
+    else:
+        print
+        print(main.__doc__)
+        return 1
+
     if parse_mode is None:
-        auto_init(filename)
+        parse_mode = auto_mode_select(filename)
+    parse_init(parse_mode, join_condition, exclude_condition)
     deck = xml_decoder(filename)
+    if inverse:
+        deck = list(deck).extend(list(map(reversed,deck))) if doublicate else list(map(reversed,deck))
     txt_encoder(filename+'_deck.txt', deck)
-    print(*deck, sep='\n')
     return 0
 
+
 if __name__ == "__main__":
-    n_args = len(sys.argv)-1
-    if n_args==1:
-        status=main(sys.argv[1])
-    else:
-        status=main()
+    status=main()
     sys.exit(status)
